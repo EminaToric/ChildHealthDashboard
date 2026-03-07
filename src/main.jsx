b5a8656d434baf4c869566353e99e77289520b13
Emina Toric <137021140+EminaToric@users.noreply.github.com>
2026-03-06 21:50:27 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import ChildHealthDashboard from "./ChildHealthDashboard"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChildHealthDashboard />
+  </React.StrictMode>
+)
